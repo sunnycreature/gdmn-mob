0c1355992ad325103491358b79ab8fd01e27c832
sunnycreature <sunnycreature@gmail.com>
2021-03-05 13:38:14 +0300

    chore: add mobile-types

---
+++ packages/mobile-types/src/constants.ts
@@ -0,0 +1,22 @@
+const statusColors = ["#4575D4", "#FFA700", "#80B12C", "#06567D"];
+
+const statuses = [
+  {
+    id: 0,
+    name: "Черновик",
+  },
+  {
+    id: 1,
+    name: "Готово",
+  },
+  {
+    id: 2,
+    name: "Отправлено",
+  },
+  {
+    id: 3,
+    name: "Обработано",
+  },
+];
+
+export { statusColors, statuses };
